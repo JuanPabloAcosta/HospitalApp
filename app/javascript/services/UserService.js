db794acae299086ed0f1031a5dcd5f983f5453e4
Juan Pablo Acosta <vporx100@gmail.com>
2020-03-28 14:50:00 -0500

    fixed react errors

--- app/javascript/services/UserService.js
+++
@@ -1,5 +0,0 @@
-export default class UserService {
-  static hasRole(role) {
-    return window.currentUser.roles.includes(role);
-  }
-}
--- app/javascript/services/index.js
+++
@@ -1,4 +0,0 @@
-import HttpClient from "./httpClient";
-import UserService from "./UserService";
-
-export { HttpClient, UserService };
